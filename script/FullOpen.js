60a277476f3f28e21f0c35e7aac18ca0695cab02
chachacode <103096938+ChaChaCode@users.noreply.github.com>
2024-08-01 02:28:22 +0500

    Update FullOpen.js

--- script/FullOpen.js
+++ script/FullOpen.js
@@ -1,3 +1,10 @@
+// app.js
+window.Telegram.WebApp.ready();
+
+window.Telegram.WebApp.onEvent('themeChanged', function() {
+    // После того, как WebApp готов, разверните его
+    window.Telegram.WebApp.expand();
+    
 document.addEventListener('DOMContentLoaded', function() {
     // Проверяем, что WebApp загружен
     window.Telegram.WebApp.ready();
