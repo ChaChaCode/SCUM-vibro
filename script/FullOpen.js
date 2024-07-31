cd190662e64b282b7f8bdc5af78f432e66b0ab0c
chachacode <103096938+ChaChaCode@users.noreply.github.com>
2024-08-01 02:12:32 +0500

    Update FullOpen.js

--- script/FullOpen.js
+++ script/FullOpen.js
@@ -1,10 +1,13 @@
 // app.js
 window.Telegram.WebApp.ready();
 
+// Включение подтверждения при закрытии
+window.Telegram.WebApp.isClosingConfirmationEnabled = true;
+
 window.Telegram.WebApp.onEvent('themeChanged', function() {
     // После того, как WebApp готов, разверните его
     window.Telegram.WebApp.expand();
 });
 
-// Вы также можете вызвать expand() сразу после того, как приложение загрузилось
+// Разворачиваем WebApp сразу после загрузки
 window.Telegram.WebApp.expand();
