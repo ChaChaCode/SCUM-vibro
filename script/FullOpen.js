8d3bd7b4a21bb8c154285bb153796bdc99d7bd60
chachacode <103096938+ChaChaCode@users.noreply.github.com>
2024-08-01 02:25:41 +0500

    Update FullOpen.js

--- script/FullOpen.js
+++ script/FullOpen.js
@@ -1,26 +1,10 @@
-// app.js
-window.Telegram.WebApp.ready();
+document.addEventListener('DOMContentLoaded', function() {
+    // Проверяем, что WebApp загружен
+    window.Telegram.WebApp.ready();
 
-// Включение подтверждения при закрытии
-window.Telegram.WebApp.isClosingConfirmationEnabled = true;
+    // Включение подтверждения при закрытии
+    window.Telegram.WebApp.isClosingConfirmationEnabled = true;
 
-// Запрос доступа на запись
-window.Telegram.WebApp.requestWriteAccess().then((result) => {
-    if (result) {
-        console.log('Доступ на запись предоставлен');
-        // Теперь вы можете выполнять действия, требующие доступа на запись
-    } else {
-        console.log('Доступ на запись не предоставлен');
-        // Обработка случая, когда доступ не был предоставлен
-    }
-}).catch((error) => {
-    console.error('Ошибка запроса доступа на запись:', error);
-});
-
-window.Telegram.WebApp.onEvent('themeChanged', function() {
-    // После того, как WebApp готов, разверните его
+    // Разворачиваем WebApp на весь экран
     window.Telegram.WebApp.expand();
 });
-
-// Вы также можете вызвать expand() сразу после того, как приложение загрузилось
-window.Telegram.WebApp.expand();
