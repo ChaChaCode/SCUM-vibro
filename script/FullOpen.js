66c1d99e3115e7c0496cf03018cc4aab48ca6d00
chachacode <103096938+ChaChaCode@users.noreply.github.com>
2024-08-01 02:19:11 +0500

    Update FullOpen.js

--- script/FullOpen.js
+++ script/FullOpen.js
@@ -4,6 +4,19 @@ window.Telegram.WebApp.ready();
 // Включение подтверждения при закрытии
 window.Telegram.WebApp.isClosingConfirmationEnabled = true;
 
+// Запрос доступа на запись
+window.Telegram.WebApp.requestWriteAccess().then((result) => {
+    if (result) {
+        console.log('Доступ на запись предоставлен');
+        // Теперь вы можете выполнять действия, требующие доступа на запись
+    } else {
+        console.log('Доступ на запись не предоставлен');
+        // Обработка случая, когда доступ не был предоставлен
+    }
+}).catch((error) => {
+    console.error('Ошибка запроса доступа на запись:', error);
+});
+
 window.Telegram.WebApp.onEvent('themeChanged', function() {
     // После того, как WebApp готов, разверните его
     window.Telegram.WebApp.expand();
