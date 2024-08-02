7b889ab880426d79cc7cb8d784666a765d4fe14a
chachacode <103096938+ChaChaCode@users.noreply.github.com>
2024-08-02 17:48:40 +0500

    Update upgrade.js

--- script/upgrade.js
+++ script/upgrade.js
@@ -18,67 +18,77 @@ if (faceBattonContainer) {
   });
 }
 
-function updateModalSize() {
-    var modals = document.querySelectorAll('.modal');
-    modals.forEach(function(modal) {
-        var modalContent = modal.querySelector(".modal-content");
-        if (modalContent) {
-            var width = window.innerWidth;
-            var scale;
-            var bottom;
+function updateModalSize(modalContent) {
+    var width = window.innerWidth;
+    var scale;
+    var bottom;
 
-            if (width <= 360) {
-                scale = 0.78;
-                bottom = '-65px';
-            } else if (width <= 375) {
-                scale = 0.82;
-                bottom = '-53px';
-            } else if (width <= 390) {
-                scale = 0.85;
-                bottom = '-43px';
-            } else if (width <= 412) {
-                scale = 0.90;
-                bottom = '-30px';
-            } else if (width <= 414) {
-                scale = 0.90;
-                bottom = '-10px';
-            } else if (width <= 430) {
-                scale = 0.94;
-                bottom = '-17px';
-            } else if (width <= 450) {
-                scale = 0.95;
-                bottom = '-15px';
-            } else {
-                scale = 1;
-                bottom = '0';
-            }
+    if (width <= 360) {
+        scale = 0.78;
+        bottom = '-65px';
+    } else if (width <= 375) {
+        scale = 0.82;
+        bottom = '-53px';
+    } else if (width <= 390) {
+        scale = 0.85;
+        bottom = '-43px';
+    } else if (width <= 412) {
+        scale = 0.90;
+        bottom = '-30px';
+    } else if (width <= 414) {
+        scale = 0.90;
+        bottom = '-10px';
+    } else if (width <= 430) {
+        scale = 0.94;
+        bottom = '-17px';
+    } else if (width <= 450) {
+        scale = 0.95;
+        bottom = '-15px';
+    } else {
+        scale = 1;
+        bottom = '0';
+    }
 
-            modalContent.style.transform = 'translateX(-50%) scale(' + scale + ')';
-            modalContent.style.bottom = bottom;
-        }
-    });
+    modalContent.style.transform = 'translateX(-50%) scale(' + scale + ')';
+    modalContent.style.bottom = bottom;
 }
 
 function openModal(modalId) {
     var modal = document.getElementById(modalId);
+    var modalContent = modal.querySelector(".modal-content");
+
     if (modal) {
         modal.style.display = "block";
-        updateModalSize(); // Обновляем размер модального окна при открытии
+        gsap.fromTo(modal, 
+            { opacity: 0 }, 
+            { opacity: 1, duration: 0.3 }
+        );
+        gsap.fromTo(modalContent, 
+            { opacity: 0, y: 500 }, 
+            { opacity: 1, y: 0, duration: 0.3, ease: 'power3.inOut' }
+        );
     }
+    updateModalSize(modalContent);
 }
 
 function closeModal(modalId) {
     var modal = document.getElementById(modalId);
     var modalContent = modal.querySelector(".modal-content");
+
     if (modal) {
-        modalContent.classList.add("close-animation");
-        setTimeout(function() {
-            modal.style.display = "none";
-            modalContent.classList.remove("close-animation");
-        }, 300); // Длительность анимации в мс
+        gsap.to(modalContent, 
+            { opacity: 0, y: 500, duration: 0.3, ease: 'power3.inOut', onComplete: function() {
+                modal.style.display = "none";
+            } }
+        );
+        gsap.to(modal, 
+            { opacity: 0, duration: 0.3 }
+        );
     }
+    updateModalSize(modalContent);  
 }
 
+// Привязка обработчиков событий
 document.getElementById("multitapGroupContainer").onclick = function() {
     openModal("multitapModal");
 }
@@ -103,6 +113,7 @@ document.getElementById("tapbotClose").onclick = function() {
     closeModal("tapbotModal");
 }
 
+// Закрытие модального окна при клике вне его
 window.onclick = function(event) {
     if (event.target.classList.contains("modal")) {
         closeModal(event.target.id);
@@ -113,15 +124,6 @@ window.addEventListener('touchend', function(event) {
     if (event.target.classList.contains("modal")) {
         closeModal(event.target.id);
     }
-});
-
-window.addEventListener('resize', function() {
-    var modals = document.querySelectorAll('.modal');
-    modals.forEach(function(modal) {
-        if (modal.style.display === "block") {
-            updateModalSize(); // Обновляем размер при изменении размера окна
-        }
-    });
 });
 
 window.addEventListener('load', function() {
