6a0e8e05f926d20b1cf7c81868c59957d4b98a62
chachacode <103096938+ChaChaCode@users.noreply.github.com>
2024-08-08 03:15:53 +0500

    Update upgrade.js

--- script/upgrade.js
+++ script/upgrade.js
@@ -112,31 +112,30 @@ window.addEventListener('load', function() {
         el.style.filter = 'none'; // Убираем размытие после загрузки
     });
 
-
-    // Проверка и установка поддержки вибрации
-    if (Telegram.WebApp.HapticFeedback) {
-        function handleButtonClick(url) {
-            return function (e) {
-                e.preventDefault();
-                // Запуск вибрации
-                Telegram.WebApp.HapticFeedback.impactOccurred('medium');
-                // Переход по ссылке
-                window.location.href = url;
-            };
-        }
-    
-        function setupButton(id, url) {
-            var button = document.getElementById(id);
-            if (button) {
-                button.addEventListener("click", handleButtonClick(url));
+        // Проверка и установка поддержки вибрации
+        if (Telegram.WebApp.HapticFeedback) {
+            function handleButtonClick(url) {
+                return function (e) {
+                    e.preventDefault();
+                    // Запуск вибрации
+                    Telegram.WebApp.HapticFeedback.impactOccurred('medium');
+                    // Переход по ссылке
+                    window.location.href = url;
+                };
             }
+        
+            function setupButton(id, url) {
+                var button = document.getElementById(id);
+                if (button) {
+                    button.addEventListener("click", handleButtonClick(url));
+                }
+            }
+        
+            setupButton("faceBattonContainer", "./invite.html");
+            setupButton("groupInfoTextContainer", "./info.html");
+            setupButton("TaskBattonContainer", "./task.html");
+            setupButton("TopStarsBattonContainer", "./leadersbord.html");
+            setupButton("rukaBattonContainer", "./index.html");
+        
         }
-    
-        setupButton("faceBattonContainer", "./invite.html");
-        setupButton("groupInfoTextContainer", "./info.html");
-        setupButton("TaskBattonContainer", "./task.html");
-        setupButton("TopStarsBattonContainer", "./leadersbord.html");
-        setupButton("rukaBattonContainer", "./index.html");
-    
-    }
 });
