4d998611815d9f6611733ba6a2a69e76ab9ff113
chachacode <103096938+ChaChaCode@users.noreply.github.com>
2024-08-08 00:12:02 +0500

    Update FullOpen.js

--- script/FullOpen.js
+++ script/FullOpen.js
@@ -8,10 +8,16 @@ document.addEventListener('DOMContentLoaded', function() {
     // Разворачиваем WebApp на весь экран
     window.Telegram.WebApp.expand();
 
+    // Устанавливаем цвет фона заголовка
+    window.Telegram.WebApp.setHeaderColor('bg_color', '#ff5733'); // Замените '#ff5733' на нужный вам цвет
+
     // Подписка на событие themeChanged
     window.Telegram.WebApp.onEvent('themeChanged', function() {
         // Обработка изменения темы, если нужно
         // Например, можно повторно развернуть WebApp на весь экран
         window.Telegram.WebApp.expand();
+
+        // Можно также обновить цвет заголовка при изменении темы, если нужно
+        window.Telegram.WebApp.setHeaderColor('bg_color', '#ff5733'); // Замените '#ff5733' на нужный вам цвет
     });
 });
